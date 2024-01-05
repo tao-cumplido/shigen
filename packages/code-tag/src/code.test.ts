22b0eb3253d7d1f7c84854952b9a11881ea3bddd
Tao Cumplido <taocumplido@gmail.com>
2024-01-05 16:36:46 +0100

    code-tag wip

---
+++ packages/code-tag/src/code.test.ts
@@ -0,0 +1,32 @@
+import assert from 'node:assert/strict';
+import { test } from 'node:test';
+
+import dedent from 'dedent';
+
+import { code } from './code.js';
+
+test('string', () => {
+	assert.equal(
+		code.js`
+			a
+			${'b'}
+		`,
+		dedent`
+			a
+			"b"
+		`,
+	);
+});
+
+test('literal', () => {
+	assert.equal(
+		code.js`
+			a
+			${code.literal('b')}
+		`,
+		dedent`
+			a
+			b
+		`,
+	);
+});
---
+++ packages/code-tag/src/code.ts
@@ -0,0 +1,29 @@
+import type { JsonValue } from 'type-fest';
+import dedent from 'dedent';
+
+class CodeLiteral {
+	readonly value: string;
+
+	constructor(value: string) {
+		this.value = value;
+	}
+}
+
+export function code<T extends JsonValue = JsonValue>(apply: (value: T) => string) {
+	return (strings: TemplateStringsArray, ...dynamics: ReadonlyArray<T | CodeLiteral>): string => {
+		return dedent(
+			strings,
+			...dynamics.map((dynamic) => {
+				if (dynamic instanceof CodeLiteral) {
+					return dynamic.value;
+				}
+
+				return apply(dynamic);
+			}),
+		);
+	};
+}
+
+code.literal = (value: string) => new CodeLiteral(value);
+
+code.js = code((value) => JSON.stringify(value));
