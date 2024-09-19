ac9351d4ee80e05ab3b6ef03a9c63971a295e9a2
Tao Cumplido <taocumplido@gmail.com>
2024-09-19 21:31:49 +0200

    eslint-plugin: more strict plugin typed export

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,14 +1,13 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import type { ESLint } from 'eslint';
 
 import { rule as groupImports } from './rules/group-imports';
 import { rule as sortImports } from './rules/sort-imports';
 
-const plugin: ESLint.Plugin = {
+const plugin = {
 	rules: {
 		'group-imports': groupImports,
 		'sort-imports': sortImports,
 	},
-};
+} satisfies ESLint.Plugin;
 
 export default plugin;
